41d424bd9a8845ed5c0bfa6c7a653ad28b567209
GrzegorzGodlewski <39514025+GrzegorzGodlewski@users.noreply.github.com>
2019-04-16 20:51:52 +0200

    filtering request in progress

--- resources/views/posts/texter_search.blade.php
+++ resources/views/posts/texter_search.blade.php
@@ -18,16 +18,13 @@
 		</tr>
 	</thead>
 	<tbody>
-		
+		<tr></tr>
 	</tbody>
 </table>
 
 
-<script src="http://code.jquery.com/jquery-3.3.1.min.js"
-               integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="
-               crossorigin="anonymous">
-</script>
-<script type="text/javascript">
+    <script src="https://code.jquery.com/jquery-1.9.1.min.js"></script>
+<script>
 	$(document).ready(function(){
 
 		fetch_post_data();
